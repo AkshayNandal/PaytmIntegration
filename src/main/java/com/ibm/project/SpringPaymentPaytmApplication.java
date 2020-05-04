a6cad50089fa90c3df0e2e87af7a2eeceb0c1db5
Akshay <nandal.akshay@yahoo.com>
2020-05-04 13:07:05 +0530

    First Commit

---
+++ src/main/java/com/ibm/project/SpringPaymentPaytmApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaymentPaytmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaymentPaytmApplication.class, args);
+	}
+
+}
